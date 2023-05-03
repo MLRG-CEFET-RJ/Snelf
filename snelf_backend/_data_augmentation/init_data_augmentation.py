700533004637e56208b8956501fadc5b8e7e6be8
kassiopereirabm <kassio.lima.p@gmail.com>
2023-05-02 21:35:53 -0300

    Update init_data_augmentation.py

--- snelf_backend/_data_augmentation/init_data_augmentation.py
+++ snelf_backend/_data_augmentation/init_data_augmentation.py
@@ -4,15 +4,20 @@ from . import anvisa_pa_augmentation
 import _log_debug.logger as logger
 
 def run():
-    logger.log("init_data_augmentation --> medicamentos_augmentation iniciado")
-    medicamentos_augmentation.run()
-    logger.log("init_data_augmentation --> medicamentos_augmentation finalizado")
-    logger.log("init_data_augmentation --> anvisa_prod_augmentation iniciado")
-    anvisa_prod_augmentation.run()
-    logger.log("init_data_augmentation --> anvisa_prod_augmentation finalizado")
-    logger.log("init_data_augmentation --> anvisa_pa_augmentation iniciado")
-    anvisa_pa_augmentation.run()
-    logger.log("init_data_augmentation --> anvisa_pa_augmentation finalizado")
+    if ("init_data_augmentation iniciado" in logger.lastLine()) or ("init_data_augmentation --> medicamentos_augmentation iniciado" in logger.lastLine()):
+        logger.log("init_data_augmentation --> medicamentos_augmentation iniciado")
+        medicamentos_augmentation.run()
+        logger.log("init_data_augmentation --> medicamentos_augmentation finalizado")
+
+    if ("init_data_augmentation --> medicamentos_augmentation finalizado" in logger.lastLine()) or ("init_data_augmentation --> anvisa_prod_augmentation iniciado" in logger.lastLine()):
+        logger.log("init_data_augmentation --> anvisa_prod_augmentation iniciado")
+        anvisa_prod_augmentation.run()
+        logger.log("init_data_augmentation --> anvisa_prod_augmentation finalizado")
+
+    if ("init_data_augmentation --> anvisa_prod_augmentation finalizado" in logger.lastLine()) or ("init_data_augmentation --> anvisa_pa_augmentation iniciado" in logger.lastLine()):
+        logger.log("init_data_augmentation --> anvisa_pa_augmentation iniciado")
+        anvisa_pa_augmentation.run()
+        logger.log("init_data_augmentation --> anvisa_pa_augmentation finalizado")
 
 
 # run()
