a7d5be17b2176b1e73646c2833adc7271158e734
Gabriel Franco <francobarreto1708@gmail.com>
2024-10-27 00:27:59 -0300

    trocando a base url para localhost

--- snelf_frontend/src/config/config.ts
+++ snelf_frontend/src/config/config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://b869-177-12-8-102.ngrok-free.app';
+// export const BASE_URL = "https://b869-177-12-8-102.ngrok-free.app";
+export const BASE_URL = "http://localhost:7768";
